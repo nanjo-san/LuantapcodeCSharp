119275c08e3e508fa156a366d014c87357a7f4c1
nanjo-san <luan.nth.63cntt@ntu.edu.vn>
2023-03-24 00:39:10 +0700

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+namespace PeoPle
+{
+    public class InfoMaTion
+    {
+        //khai bao truong
+        private string id;
+        private string name;
+        private int age;
+        private string country;
+        //query and update
+        public string ID
+        {
+            get { return id; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    id = value;
+                }
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (age != 0 && age > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    country = value;
+                }
+            }
+        }
+        //thiet lap ban dau 
+        public InfoMaTion()
+        {
+            id = "2154545";
+            name = "Nguyen van A";
+            age = 1;
+            country = "Phu yen";
+        }
+        //phuong thuc Nhap
+        public void Nhap()
+        {
+            Console.Write("Import ID CCCD/CMD :  ");
+            id = Convert.ToString(Console.ReadLine());
+            Console.Write("Import Your Name : ");
+            name = Convert.ToString(Console.ReadLine());
+            Console.Write("Import Your Age : ");
+            age = int.Parse(Console.ReadLine());
+            Console.Write("Import Your Country : ");
+            country = Convert.ToString(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            Console.WriteLine(" Thong tin CCCD/CMND =  {0} + Họ và Tên =  {1} + Tuổi = {2} + Country = {3}", id, name, age, country);
+        }
+    }
+    public class ListInfomation
+    {
+        int n; //So luong nguoi can nhap thong tin
+        InfoMaTion[] Listinfo;
+        public void Nhap()
+        {
+            Console.Write("Enter the number of person to fill in information :  ");
+            n = int.Parse(Console.ReadLine());
+            Listinfo = new InfoMaTion[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("PerSon {0} ", i + 1);
+                Listinfo[i] = new InfoMaTion();
+                Listinfo[i].Nhap();
+            }
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("Number of person entered :");
+            for(int i = 0; i < n; i++)
+            {
+                Listinfo[i].Xuat();
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ListInfomation info = new ListInfomation();
+            info.Nhap();
+            info.Xuat();
+        }
+    }
+}
---
+++ PhanSo/PhanSo/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+namespace Appp1
+{
+    class PhanSo
+    {
+        //cac truong luu du lieu cua bien
+        private int tuSo;
+        private int mauSo;
+        //query and update
+        public int TuSo
+        {
+            get { return tuSo; }
+            set
+            {
+                if (mauSo != 0)
+                {
+                    tuSo = value;
+                }
+            }
+        }
+        public int MauSo
+        {
+            get { return mauSo; }
+            set
+            {
+                if (mauSo != 0)
+                {
+                    mauSo = value;
+                }
+            }
+        }
+        //thiet lap ban dau 
+        public PhanSo(int ts = 0, int ms = 0)
+        {
+            this.tuSo = ts;
+            this.mauSo = ms;
+        }
+        //phuong thuc Nhap
+        public void Nhap()
+        {
+            Console.Write("Nhap tu so : ");
+            tuSo = int.Parse(Console.ReadLine());
+            Console.Write("Nhap mau so : ");
+            mauSo = int.Parse(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            Console.Write("\nPhan so vua nhap :{0}/{1} \n ", tuSo, mauSo);
+        }
+        public double GetValue()
+        {
+            return (double)tuSo / mauSo;
+        }
+        public void ToiGian()
+        {
+            int uc = UCLN.ucln(tuSo, mauSo);
+            tuSo /= uc;
+            mauSo /= uc;
+        }
+    }
+    class DSPhanSo
+    {
+        int n; //so luong phan tu
+        PhanSo[] ListPhanSo;
+        public void Nhap()
+        {
+            Console.Write("Nhap so luong phan tu : ");
+            n = int.Parse(Console.ReadLine());
+            ListPhanSo = new PhanSo[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Phan so thu {0}", i + 1);
+                ListPhanSo[i] = new PhanSo();
+                ListPhanSo[i].Nhap();
+            }
+        }
+        public void Xuat()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                ListPhanSo[i].Xuat();
+            }
+        }
+        public void ToiGian()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                ListPhanSo[i].ToiGian();
+                ListPhanSo[i].Xuat();
+            }
+        }
+        public void PhanSoMax()
+        {
+            int index = 0;
+            double max = ListPhanSo[0].GetValue();
+            for (int i = 1; i < n; i++)
+            {
+                if (ListPhanSo[i].GetValue() > max)
+                {
+                    max = ListPhanSo[i].GetValue();
+                }
+            }
+            Console.Write("Phan So Lon Nhat Cua Mang : {0}\n", max);
+        }
+        public void swap(double a, double b)
+        {
+            double temp = a;
+            a = b;
+            b = temp;
+        }
+        public void SapXep()
+        {
+            double max = ListPhanSo[0].GetValue();
+            for (int i = 1; i < n; i++)
+            {
+                if (ListPhanSo[i].GetValue() > max)
+                {
+                    swap(ListPhanSo[i].GetValue(), max);
+                }
+            }
+            Console.WriteLine("Mang sau khi duoc sap xep la : ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(ListPhanSo[i].GetValue() + " ");
+            }
+        }
+    }
+    public static class UCLN
+    {
+        public static int ucln(int a, int b)
+        {
+            if (a == 0 || b == 0) return a + b;
+            while (a != b)
+            {
+                if (a > b) a = a - b; else b = b - a;
+            }
+            return a;
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            PhanSo ps = new PhanSo();
+            ps.Nhap();
+            ps.Xuat();
+            ps.GetValue();
+            DSPhanSo dsps = new DSPhanSo();
+            dsps.Nhap();
+            dsps.Xuat();
+            Console.WriteLine("\nPhan so sau khi toi gian");
+            dsps.ToiGian();
+            dsps.PhanSoMax();
+            dsps.SapXep();
+        }
+    }
+}
